1ac539b7367a78497570811e3c5a846b07831bf8
majharul <majharuljony163@gmail.com>
2022-10-24 12:06:05 +0600

    fix issu in home page

--- js/app.js
+++ js/app.js
@@ -85,6 +85,30 @@ $(".review").slick({
     return '<div class="pager__item" id=' + i + "> __ </div>";
   },
 });
+
+$(".success-slide").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
 // back to top
 let scrollBtn = document.getElementById("scrollBtn");
 
